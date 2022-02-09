af37ce31b2bf4bf141c1ea6340d2e3184542b265
0captjack <85979485+0captjack@users.noreply.github.com>
2022-02-09 09:55:45 -0700

    Added 3 models

---
+++ Mission6/Models/TaskContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskContext : DbContext
+    {
+
+        public TaskContext (DbContextOptions <TaskContext> options) : base (options)
+        {
+
+        }
+        public DbSet<TaskModel> responses { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //seeding the database
+            mb.Entity<TaskModel>().HasData(
+                new TaskModel
+                {
+                    Task = "Sweep the Floor",
+                    Date = "02/11/22",
+                    Quadrant = "3",
+                    CategoryId = 0,
+                    Completed = false
+                }
+                new TaskModel
+                {
+                    Task = "Clean the Microwave",
+                    Date = "02/11/22",
+                    Quadrant = "2",
+                    CategoryId = 0,
+                    Completed = false
+                }
+            );
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Home" },
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+            );
+        }
+    }
+}
---
+++ Mission6/Models/TaskModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        [Required]
+        public string Task { get; set; }
+
+        public string Date { get; set; }
+        [Required(ErrorMessage = "You must enter a Quadrant for the Task")]
+        public int Quadrant { get; set; }
+
+        public Category Category { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
